e45b2364730ca82c7d4c1208744202d2d4f982b1
Jessica Needham <jessica@login-1.betzy>
2026-04-24 14:40:27 +0200

    update AD spinup to use default branchces

--- runscripts/create_fates_LU_spinup_AD.sh
+++ runscripts/create_fates_LU_spinup_AD.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 export COMPSET='1850_DATM%CRUJRA2024_CLM60%FATES_SICE_SOCN_SROF_SGLC_SWAV_SESP'
-export RES="ne16pg3_tn14"   # "f45_f45_mg37" # ne16pg3_tn14, #f19_g17, ne30pg3_tn14, f45_f45_mg37, ne16pg3_tn14
+export RES=ne16pg3_tn14   # f45_f45_mg37,  f19_g17, ne30pg3_tn14
 export MACH='betzy'
-export PROJECT='nn9188k'
+export PROJECT='nn9560k'
 
 export USER='jessica'
 export workpath='/cluster/work/users/jessica'
-export paramdir='/cluster/home/jessica/NCSrevise/paramfiles'
 
-export TAG='noresm-fates-ne16-LU-PPE-AD-spinup_v2'
+export TAG='noresm-fates-ne16-LU-PPE-AD-spinup'
 export CASEROOT=$workpath/ncsrevise_runs
-export CIMEROOT=$workpath/noresm-ncsrevise/CTSM/cime/scripts
+export CIMEROOT=$workpath/noresm-def/CTSM/cime/scripts
 
 cd ${CIMEROOT}
 
@@ -20,7 +19,7 @@ export NorESM_CTSM_HASH=`(cd ../..;git log -n 1 --pretty=%h)`
 echo $PWD
 export FATES_HASH=`(cd src/fates;git log -n 1 --pretty=%h)`
 export GIT_HASH=N${NorESM_CTSM_HASH}-F${FATES_HASH}	
-export CASE_NAME=${CASEROOT}/${TAG}.${GIT_HASH}.`date +"%Y-%m-%d"`
+export CASE_NAME=${CASEROOT}/${TAG}.`date +"%Y-%m-%d"`
 
 
 # REMOVE EXISTING CASE DIRECTORY IF PRESENT 
@@ -32,11 +31,14 @@ rm -rf ${CASE_NAME}
 cd ${CASE_NAME}
 
 # 
+#./xmlchange STOP_N=50
 ./xmlchange STOP_N=50
 ./xmlchange STOP_OPTION=nyears
+#./xmlchange REST_N=25
 ./xmlchange REST_N=25
 ./xmlchange REST_OPTION=nyears
-./xmlchange RESUBMIT=3
+#./xmlchange RESUBMIT=7
+./xmlchange RESUBMIT=7
 ./xmlchange DEBUG=FALSE
 
 ./xmlchange RUN_STARTDATE=0001-01-01
@@ -49,7 +51,7 @@ cd ${CASE_NAME}
 
 
 # turn on megan for NCSrevise runs
-./xmlchange CLM_BLDNML_OPTS="-bgc fates -megan"
+#./xmlchange CLM_BLDNML_OPTS="-bgc fates -megan"
 
 # For real runs
 ./xmlchange --subgroup case.run JOB_WALLCLOCK_TIME=24:00:00
@@ -63,33 +65,29 @@ cd ${CASE_NAME}
 ./xmlchange RUNDIR=${CASE_NAME}/run
 ./xmlchange EXEROOT=${CASE_NAME}/bld
 
+# note with land use on we don't need the surface datafile but not
+# specifying it leads to an error because the default file for
+# this grid doesn't exist. 
 
 cat >>  user_nl_clm <<EOF
-fates_paramfile='/cluster/home/jessica/NCSrevise/paramfiles/fates_params_sci.1.88.6_api.42.0.0_14pft_nor_sci2_api1_c251212_LU_updates.nc'
+fates_paramfile='/cluster/home/jessica/NCSrevise/paramfiles/fates_params_pr52.nc'
 use_fates_sp=.false.
 use_fates_nocomp=.true.
 use_fates_fixed_biogeog=.true.
-fates_stomatal_model='medlyn2011'
-fates_radiation_model = 'twostream'
-fates_leafresp_model = 'ryan1991'
 use_fates_luh=.true.
 use_fates_lupft=.true.
 fates_harvest_mode='luhdata_area'
 use_fates_potentialveg=.false.
 fluh_timeseries='/cluster/shared/noresm/inputdata/LU_data_CMIP7/LUH2_states_transitions_management.timeseries_ne16_hist_steadystate_1850_2025-11-06_cdf5.nc'
 flandusepftdat='/cluster/shared/noresm/inputdata/LU_data_CMIP7/fates_landuse_pft_map_to_surfdata_ne16np4_251106_cdf5.nc'
-fates_spitfire_mode=4
-fates_lu_transition_logic=1
 hist_empty_htapes=.true.
 hist_fincl1='FCO2', 'FATES_SECONDARY_AREA_ANTHRO_AP','FATES_SECONDARY_AREA_AP','FATES_PRIMARY_AREA_AP','FATES_NPP_LU','FATES_GPP_LU',
 'FATES_VEGC_PF', 'FATES_VEGC_LU', 'FATES_LAI', 'FATES_GPP_PF'
 EOF
 
-
-#cat >> user_nl_datm <<EOF
-#taxmode = "cycle", "cycle", "cycle"
-#EOF
-
 ./case.setup
 ./case.build
 ./case.submit
+
+#fluh_timeseries='/cluster/work/users/jessica/trendy_lu_files_2degs/LUH2_states_transitions_management.timeseries_1.9x2.5_hist_steadystate_1700_2025-07-23_cdf5.nc'
+#flandusepftdat='/cluster/work/users/jessica/trendy_lu_files_2degs/fates_landuse_pft_map_to_surfdata_1.9x2.5_250723_cdf5.nc'
