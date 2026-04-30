ea04efe4667eecf5ebf371ef2aed1ef139632f56
Jessica Needham <jessica@login-1.betzy>
2026-04-30 09:44:29 +0200

    update post-AD runscript

--- runscripts/create_fates_LU_spinup_postAD.sh
+++ runscripts/create_fates_LU_spinup_postAD.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 export COMPSET='1850_DATM%CRUJRA2024_CLM60%FATES_SICE_SOCN_SROF_SGLC_SWAV_SESP'
-export RES="ne16pg3_tn14"   # "f45_f45_mg37" # ne16pg3_tn14, #f19_g17, ne30pg3_tn14, f45_f45_mg37, ne16pg3_tn14
+export RES=ne16pg3_tn14
 export MACH='betzy'
-export PROJECT='nn9188k'
+export PROJECT='nn9560k'
 
 export USER='jessica'
 export workpath='/cluster/work/users/jessica'
-export paramdir='/cluster/home/jessica/NCSrevise/paramfiles'
 
-export TAG='noresm-fates-ne16-LU-PPE-postAD-spinup'
+export TAG='noresm-fates-ne16_LU-PPE-postAD-spinup'
 export CASEROOT=$workpath/ncsrevise_runs
-export CIMEROOT=$workpath/noresm-ncsrevise/CTSM/cime/scripts
+export CIMEROOT=$workpath/noresm-def/CTSM/cime/scripts
 
 cd ${CIMEROOT}
 
@@ -19,7 +18,7 @@ export CIME_HASH=`git log -n 1 --pretty=%h`
 export NorESM_CTSM_HASH=`(cd ../..;git log -n 1 --pretty=%h)`
 export FATES_HASH=`(cd src/fates;git log -n 1 --pretty=%h)`
 export GIT_HASH=N${NorESM_CTSM_HASH}-F${FATES_HASH}	
-export CASE_NAME=${CASEROOT}/${TAG}.${GIT_HASH}.`date +"%Y-%m-%d"`
+export CASE_NAME=${CASEROOT}/${TAG}.`date +"%Y-%m-%d"`
 
 
 # REMOVE EXISTING CASE DIRECTORY IF PRESENT 
@@ -31,14 +30,14 @@ rm -rf ${CASE_NAME}
 cd ${CASE_NAME}
 
 # For the real thing make this 200 years (100 years with one resubmit)
-./xmlchange STOP_N=50
+./xmlchange STOP_N=25
 ./xmlchange STOP_OPTION=nyears
 ./xmlchange REST_N=25
 ./xmlchange REST_OPTION=nyears
 ./xmlchange RESUBMIT=3
 ./xmlchange DEBUG=FALSE
 
-./xmlchange RUN_STARTDATE=0201-01-01 # check this matches end of AD spinup run
+./xmlchange RUN_STARTDATE=0401-01-01 # check this matches end of AD spinup run
 ./xmlchange CLM_ACCELERATED_SPINUP=off
 ./xmlchange CCSM_CO2_PPMV=287.
 ./xmlchange DATM_YR_START=1901
@@ -62,37 +61,27 @@ cd ${CASE_NAME}
 
 # use existing build
 ./xmlchange BUILD_COMPLETE=TRUE
-./xmlchange EXEROOT=/cluster/work/users/jessica/ncsrevise_runs/noresm-fates-ne16-LU-PPE-AD-spinup.N1048fbece-Fab73a7eb7.2026-01-06/bld # point to build from the ad spin up
+./xmlchange EXEROOT=/cluster/work/users/jessica/ncsrevise_runs/noresm-fates-ne16-LU-PPE-AD-spinup.2026-04-24/bld
 
 cat >>  user_nl_clm <<EOF
-finidat='/cluster/work/users/jessica/ncsrevise_runs/noresm-fates-ne16-LU-PPE-AD-spinup.N1048fbece-Fab73a7eb7.2026-01-06/run/'
-fates_paramfile='/cluster/home/jessica/NCSrevise/paramfiles/fates_params_sci.1.88.6_api.42.0.0_14pft_nor_sci2_api1_c251212_LU_updates.nc'
+finidat='/cluster/work/users/jessica/ncsrevise_runs/noresm-fates-ne16-LU-PPE-AD-spinup.2026-04-24/run/noresm-fates-ne16-LU-PPE-AD-spinup.2026-04-24.clm2.r.0401-01-01-00000.nc'
+fates_paramfile='/cluster/home/jessica/NCSrevise/paramfiles/fates_params_pr52.nc'
 use_fates_sp=.false.
 use_fates_nocomp=.true.
 use_fates_fixed_biogeog=.true.
-fates_stomatal_model='medlyn2011'
-fates_radiation_model = 'twostream'
-fates_leafresp_model = 'ryan1991'
 use_fates_luh=.true.
 use_fates_lupft=.true.
 fates_harvest_mode='luhdata_area'
 use_fates_potentialveg=.false.
-fates_lu_transition_logic=1
 fluh_timeseries='/cluster/shared/noresm/inputdata/LU_data_CMIP7/LUH2_states_transitions_management.timeseries_ne16_hist_steadystate_1850_2025-11-06_cdf5.nc'
 flandusepftdat='/cluster/shared/noresm/inputdata/LU_data_CMIP7/fates_landuse_pft_map_to_surfdata_ne16np4_251106_cdf5.nc'
 fates_spitfire_mode=4
 hist_empty_htapes=.true.
-hist_fincl1='FCO2', 'FATES_GPP_LU', 'FATES_DISTURBANCE_RATE_MATRIX_LULU', 'FATES_TRANSITION_MATRIX_LULU', 'FATES_BURNEDAREA_LU',
+hist_fincl1='FCO2', 'FATES_GPP_LU', 
 'FATES_VEGC_LU', 'FATES_PATCHAREA_LU', 'FATES_NPP_LU', 'FATES_LUCHANGE_WOODPROD_C_FLUX', 'FATES_DISTURBANCE_RATE_LOGGING',
 'FATES_VEGC_ABOVEGROUND', 'FATES_VEGC', 'FATES_FRACTION', 'FATES_GPP','FATES_NEP','FATES_AUTORESP', 'FATES_HET_RESP', 'QVEGE',
- 'QVEGT','QSOIL','EFLX_LH_TOT','FSH','FSR', 'FSDS','FSA','FIRE','FLDS','FATES_LAI', 'FATES_VEGC_PF', 
-'FATES_SECONDARY_AREA_ANTHRO_AP','FATES_SECONDARY_AREA_AP','FATES_PRIMARY_AREA_AP','FATES_NPP_LU','FATES_GPP_LU'
+ 'QVEGT','QSOIL','EFLX_LH_TOT','FSH','FSR', 'FSDS','FSA','FIRE','FLDS','FATES_LAI', 'FATES_VEGC_PF'
 EOF
-
-
-#cat >> user_nl_datm <<EOF
-#taxmode = "cycle", "cycle", "cycle"
-#EOF
 
 ./case.setup
 #./case.build
