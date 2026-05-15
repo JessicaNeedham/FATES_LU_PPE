e0e8b0c093b364e27d4df17edd3289bc14b84664
Jessica Needham <jessica@uan02.head.cm.americas.sgi.com>
2026-05-15 10:31:19 +0200

    spinup script for olivia

--- runscripts/create_fates_LU_spinup_AD.sh
+++ runscripts/create_fates_LU_spinup_AD.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 export COMPSET='1850_DATM%CRUJRA2024_CLM60%FATES_SICE_SOCN_SROF_SGLC_SWAV_SESP'
-export RES="ne16pg3_tn14"   # "f45_f45_mg37" # ne16pg3_tn14, #f19_g17, ne30pg3_tn14, f45_f45_mg37, ne16pg3_tn14
-export MACH='betzy'
+export RES="f19_g17" #, ne30pg3_tn14, f45_f45_mg37, ne16pg3_tn14
+export MACH='olivia'
 export PROJECT='nn9188k'
 
 export USER='jessica'
-export workpath='/cluster/work/users/jessica'
-export paramdir='/cluster/home/jessica/NCSrevise/paramfiles'
+export workpath='/cluster/work/projects/nn9188k/jessica'
 
-export TAG='noresm-fates-ne16-LU-PPE-AD-spinup_v2'
+export TAG='noresm-fates-f19-LU-PPE-AD-spinup'
 export CASEROOT=$workpath/ncsrevise_runs
-export CIMEROOT=$workpath/noresm-ncsrevise/CTSM/cime/scripts
+export CIMEROOT=$workpath/noresm-beta16/CTSM/cime/scripts
 
 cd ${CIMEROOT}
 
@@ -20,7 +19,7 @@ export NorESM_CTSM_HASH=`(cd ../..;git log -n 1 --pretty=%h)`
 echo $PWD
 export FATES_HASH=`(cd src/fates;git log -n 1 --pretty=%h)`
 export GIT_HASH=N${NorESM_CTSM_HASH}-F${FATES_HASH}	
-export CASE_NAME=${CASEROOT}/${TAG}.${GIT_HASH}.`date +"%Y-%m-%d"`
+export CASE_NAME=${CASEROOT}/${TAG}.`date +"%Y-%m-%d"`
 
 
 # REMOVE EXISTING CASE DIRECTORY IF PRESENT 
@@ -36,7 +35,7 @@ cd ${CASE_NAME}
 ./xmlchange STOP_OPTION=nyears
 ./xmlchange REST_N=25
 ./xmlchange REST_OPTION=nyears
-./xmlchange RESUBMIT=3
+./xmlchange RESUBMIT=7
 ./xmlchange DEBUG=FALSE
 
 ./xmlchange RUN_STARTDATE=0001-01-01
@@ -65,30 +64,25 @@ cd ${CASE_NAME}
 
 
 cat >>  user_nl_clm <<EOF
-fates_paramfile='/cluster/home/jessica/NCSrevise/paramfiles/fates_params_sci.1.88.6_api.42.0.0_14pft_nor_sci2_api1_c251212_LU_updates.nc'
 use_fates_sp=.false.
 use_fates_nocomp=.true.
 use_fates_fixed_biogeog=.true.
-fates_stomatal_model='medlyn2011'
-fates_radiation_model = 'twostream'
-fates_leafresp_model = 'ryan1991'
 use_fates_luh=.true.
 use_fates_lupft=.true.
 fates_harvest_mode='luhdata_area'
 use_fates_potentialveg=.false.
-fluh_timeseries='/cluster/shared/noresm/inputdata/LU_data_CMIP7/LUH2_states_transitions_management.timeseries_ne16_hist_steadystate_1850_2025-11-06_cdf5.nc'
-flandusepftdat='/cluster/shared/noresm/inputdata/LU_data_CMIP7/fates_landuse_pft_map_to_surfdata_ne16np4_251106_cdf5.nc'
+fluh_timeseries='/cluster/work/projects/nn9560k/inputdata/LU_data_CMIP7/LUH3_1850_steadystate_0.9x1.25_c260514.nc'
+flandusepftdat='/cluster/work/projects/nn9560k/inputdata/LU_data_CMIP7/fates_landuse_pft_surfdata_1.9x2.5_c260513.nc'
 fates_spitfire_mode=4
+stream_year_first_popdens=1850
+stream_year_last_popdens=1850
+model_year_align_popdens=1850
 fates_lu_transition_logic=1
 hist_empty_htapes=.true.
 hist_fincl1='FCO2', 'FATES_SECONDARY_AREA_ANTHRO_AP','FATES_SECONDARY_AREA_AP','FATES_PRIMARY_AREA_AP','FATES_NPP_LU','FATES_GPP_LU',
 'FATES_VEGC_PF', 'FATES_VEGC_LU', 'FATES_LAI', 'FATES_GPP_PF'
 EOF
 
-
-#cat >> user_nl_datm <<EOF
-#taxmode = "cycle", "cycle", "cycle"
-#EOF
 
 ./case.setup
 ./case.build
